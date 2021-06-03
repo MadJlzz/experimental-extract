203a002454fb2b77036fb83aa73f8a2e14ca285a
Julien Klaer <klaer.julien@gmail.com>
2021-06-04 00:41:27 +0200

    Extraction URL working

--- src/main/java/com/nextgatetech/gcrdemo/engine/crawler/CrawlerEngine.java
+++ src/main/java/com/nextgatetech/gcrdemo/engine/crawler/CrawlerEngine.java
@@ -3,6 +3,12 @@ package com.nextgatetech.gcrdemo.engine.crawler;
 import com.nextgatetech.gcrdemo.engine.Engine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class CrawlerEngine implements Engine {
 
@@ -17,6 +23,13 @@ public class CrawlerEngine implements Engine {
     @Override
     public void extract() {
         logger.info(String.format("Extracting data from URL [%s]", this.url));
+        final RestTemplate client = new RestTemplate();
+        File file = client.execute(this.url, HttpMethod.GET, null, clientHttpResponse -> {
+            File ret = File.createTempFile("download", "tmp");
+            StreamUtils.copy(clientHttpResponse.getBody(), new FileOutputStream(ret));
+            return ret;
+        });
+        System.out.println("Next copy this data to gcs");
     }
 
 }
